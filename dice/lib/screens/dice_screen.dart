b579ec83e604f2861c40f80d78287e063658be25
Roman <pilipop1111@gmail.com>
2023-01-28 23:33:56 +0300

    добавил кубики

--- dice/lib/screens/dice_screen.dart
+++ dice/lib/screens/dice_screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,39 +12,84 @@ class diceScreen extends StatefulWidget {
 }
 
 class _diceScreenState extends State<diceScreen> {
+  String? firstCube;
+  String? secondCube;
+  List<Image> playingCubes = [];
+  final List<String> allCubes = [
+    "diceImage/dice1.png",
+    "diceImage/dice2.png",
+    "diceImage/dice3.png",
+    "diceImage/dice4.png",
+    "diceImage/dice5.png",
+    "diceImage/dice6.png",
+  ];
+  void rollCubes() {
+    setState(() {
+      playingCubes = [];
+    });
+    Random random = Random();
+    String c1 = allCubes[random.nextInt(allCubes.length)];
+    String c2 = allCubes[random.nextInt(allCubes.length)];
+    firstCube = c1;
+    secondCube = c2;
+    playingCubes.add(Image.asset(firstCube!));
+    playingCubes.add(Image.asset(secondCube!));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: 100,
-          child: ElevatedButton(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                "ROLL",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 250,
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemCount: playingCubes.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        child: playingCubes[index],
+                      );
+                    }),
+              ),
+              Container(
+                width: 100,
+                child: ElevatedButton(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      "ROLL",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(13),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromRGBO(
+                        221,
+                        81,
+                        80,
+                        1,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    rollCubes();
+                  },
                 ),
               ),
-            ),
-            style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(13),
-                ),
-              ),
-              backgroundColor: MaterialStateProperty.all<Color>(
-                Color.fromRGBO(
-                  221,
-                  81,
-                  80,
-                  1,
-                ),
-              ),
-            ),
-            onPressed: () {},
+            ],
           ),
         ),
       ),
