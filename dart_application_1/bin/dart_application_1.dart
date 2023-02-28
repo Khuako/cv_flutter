6d0d5b052c0159a69f9e03fea2caa8370a20d7e1
Khuako <112856450+Khuako@users.noreply.github.com>
2023-02-28 20:48:50 +0300

    Delete dart_application_1 directory

--- dart_application_1/bin/dart_application_1.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:math';
-
-import 'package:dart_application_1/dart_application_1.dart'
-    as dart_application_1;
-
-void main(List<String> arguments) {
-  List<int> digits = [1, 2, 3, 4, 5];
-}
