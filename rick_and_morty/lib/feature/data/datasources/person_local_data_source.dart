f0852a61df682bb8273b7b7f05990b0e5453383f
Roman <pilipop1111@gmail.com>
2023-03-30 01:39:18 +0300

    plants

---
+++ rick_and_morty/lib/feature/data/datasources/person_local_data_source.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:rick_and_morty/core/error/exception.dart';
+import 'package:rick_and_morty/feature/data/models/person_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class PersonLocalDataSource {
+  Future<List<PersonModel>> getLastPersonsFromCache();
+  Future<void> personsToCache(List<PersonModel> persons);
+}
+
+const CACHED_PERSONS_LIST = 'CACHED_PERSONS_LIST';
+
+class PersonLocalDataSourceImpl implements PersonLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  PersonLocalDataSourceImpl({required this.sharedPreferences});
+  @override
+  Future<List<PersonModel>> getLastPersonsFromCache() {
+    final jsonPersonsList =
+        sharedPreferences.getStringList(CACHED_PERSONS_LIST);
+    if (jsonPersonsList!.isNotEmpty) {
+      return Future.value(jsonPersonsList
+          .map((e) => PersonModel.fromJson(json.decode(e)))
+          .toList());
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> personsToCache(List<PersonModel> persons) {
+    final List<String> jsonPersonsList =
+        persons.map((e) => json.encode(e.toJson())).toList();
+    sharedPreferences.setStringList(CACHED_PERSONS_LIST, jsonPersonsList);
+    print('Persons to write Cache: ${jsonPersonsList.length}');
+    return Future.value(jsonPersonsList);
+  }
+}
---
+++ rick_and_morty/lib/feature/presentation/bloc/search_bloc/search_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/feature/domain/usecases/search_person.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/search_bloc/search_event.dart';
+import '../../../../core/error/failure.dart';
+import 'search_state.dart';
+
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const CACHED_FAILURE_MESSAGE = 'Cache Failure';
+
+class PersonSearchBloc extends Bloc<PersonSearchEvent, PersonSearchState> {
+  final SearchPerson searchPerson;
+
+  PersonSearchBloc({required this.searchPerson}) : super(PersonEmpty()) {
+    on<SearchPersons>(_onEvent);
+  }
+
+  FutureOr<void> _onEvent(
+      SearchPersons event, Emitter<PersonSearchState> emit) async {
+    emit(PersonSearchLoading());
+    final failureOrPerson =
+        await searchPerson(SearchPersonParams(query: event.personQuery));
+    emit(
+      failureOrPerson.fold(
+        (failure) => PersonSearchError(message: _mapFailureToMessage(failure)),
+        (person) => PersonSearchLoaded(persons: person),
+      ),
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHED_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected Error';
+    }
+  }
+}
